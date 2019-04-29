9029720087f19fd3cd3f8de93f076071d63f57b5
Win7 pablojr1971 <pablojr1971@googlemail.com>
2019-04-29 14:31:11 +0100

    first commit

---
+++ NewProject/Barcode.vb
@@ -0,0 +1,20 @@
+﻿Public Class Barcode
+    Property Index As Integer = 0
+    Property SectionName As String = Nothing
+    Property BarcodeValue As String = Nothing
+    Property OrigPageNo As String = Nothing
+
+    Public Sub New(Index As Integer, SectionName As String, BarcodeValue As String)
+        Me.Index = Index
+        Me.SectionName = SectionName
+        Me.BarcodeValue = BarcodeValue
+    End Sub
+
+    'Overload constructor to allow the original page number in the logs
+    Public Sub New(Index As Integer, SectionName As String, BarcodeValue As String, OrigPageNo As String)
+        Me.Index = Index
+        Me.SectionName = SectionName
+        Me.BarcodeValue = BarcodeValue
+        Me.OrigPageNo = OrigPageNo
+    End Sub
+End Class
